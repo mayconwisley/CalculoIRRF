b47a8e679480d45dffbaf6c22172ce286d7c54dd
Maycon Batista <maycon.batista@seniornortepr.com.br>
2023-05-15 17:59:23 -0300

    ajuste

--- CalculoIRRF/Modelos/Calculo/IrrfNormal.cs
+++ CalculoIRRF/Modelos/Calculo/IrrfNormal.cs
@@ -9,10 +9,30 @@ namespace CalculoIRRF.Modelos.Calculo
 {
     public class IrrfNormal
     {
-        DateTime competencia;
-        int qtdDependente;
-        decimal valorInss;
-        decimal valorBase;
-        
+        DateTime _competencia;
+        int _qtdDependente = 0;
+        decimal _valorInss = 0;
+        decimal _valorBase = 0;
+        decimal _valorSimplificado = 0;
+
+        public IrrfNormal(DateTime competencia, int qtdDependente, decimal valorInss, decimal valorBase, decimal valorSimplificado)
+        {
+            _competencia = competencia;
+            _qtdDependente = qtdDependente;
+            _valorInss = valorInss;
+            _valorBase = valorBase;
+            _valorSimplificado = valorSimplificado;
+        }
+
+        public decimal Normal(decimal porcentagem, decimal deducao)
+        {
+            decimal resultado = ((_valorBase - _valorInss - (_qtdDependente * 189.59m)) * (porcentagem / 100)) - deducao;
+            return resultado;
+        }
+        public decimal Simplificado(decimal deducao)
+        {
+            decimal resultado = (_valorBase - _valorSimplificado) - deducao;
+            return resultado;
+        }
     }
 }
