a0d96214e6ae23db9aa2ee9207b60a5d99a42a7a
Maycon Wisley <maycon.wisley@outlook.com>
2025-08-25 08:40:22 -0300

    Ajuste

--- CalculoIRRF/FrmPensao.Designer.cs
+++ CalculoIRRF/FrmPensao.Designer.cs
@@ -28,135 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.TxtValorBruto = new System.Windows.Forms.TextBox();
-            this.TxtPorcentagem = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.TxtOutrosDescontos = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.BtnCalcular = new System.Windows.Forms.Button();
-            this.RtxDescricao = new System.Windows.Forms.RichTextBox();
-            this.BtnDetalhar = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1 = new System.Windows.Forms.Label();
+            TxtValorBruto = new System.Windows.Forms.TextBox();
+            TxtPorcentagem = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            TxtOutrosDescontos = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            BtnCalcular = new System.Windows.Forms.Button();
+            RtxDescricao = new System.Windows.Forms.RichTextBox();
+            BtnDetalhar = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Valor Bruto";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(14, 10);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(65, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Valor Bruto";
             // 
             // TxtValorBruto
             // 
-            this.TxtValorBruto.Location = new System.Drawing.Point(12, 25);
-            this.TxtValorBruto.Name = "TxtValorBruto";
-            this.TxtValorBruto.Size = new System.Drawing.Size(83, 20);
-            this.TxtValorBruto.TabIndex = 0;
-            this.TxtValorBruto.Text = "0,00";
-            this.TxtValorBruto.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.TxtValorBruto.TextChanged += new System.EventHandler(this.TxtValorBruto_TextChanged);
-            this.TxtValorBruto.Enter += new System.EventHandler(this.TxtValorBruto_Enter);
-            this.TxtValorBruto.Leave += new System.EventHandler(this.TxtValorBruto_Leave);
+            TxtValorBruto.Location = new System.Drawing.Point(14, 29);
+            TxtValorBruto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            TxtValorBruto.Name = "TxtValorBruto";
+            TxtValorBruto.Size = new System.Drawing.Size(96, 23);
+            TxtValorBruto.TabIndex = 0;
+            TxtValorBruto.Text = "0,00";
+            TxtValorBruto.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            TxtValorBruto.TextChanged += TxtValorBruto_TextChanged;
+            TxtValorBruto.Enter += TxtValorBruto_Enter;
+            TxtValorBruto.Leave += TxtValorBruto_Leave;
             // 
             // TxtPorcentagem
             // 
-            this.TxtPorcentagem.Location = new System.Drawing.Point(101, 25);
-            this.TxtPorcentagem.Name = "TxtPorcentagem";
-            this.TxtPorcentagem.Size = new System.Drawing.Size(67, 20);
-            this.TxtPorcentagem.TabIndex = 1;
-            this.TxtPorcentagem.Text = "30,00";
-            this.TxtPorcentagem.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.TxtPorcentagem.TextChanged += new System.EventHandler(this.TxtPorcentagem_TextChanged);
-            this.TxtPorcentagem.Enter += new System.EventHandler(this.TxtPorcentagem_Enter);
-            this.TxtPorcentagem.Leave += new System.EventHandler(this.TxtPorcentagem_Leave);
+            TxtPorcentagem.Location = new System.Drawing.Point(118, 29);
+            TxtPorcentagem.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            TxtPorcentagem.Name = "TxtPorcentagem";
+            TxtPorcentagem.Size = new System.Drawing.Size(78, 23);
+            TxtPorcentagem.TabIndex = 1;
+            TxtPorcentagem.Text = "30,00";
+            TxtPorcentagem.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            TxtPorcentagem.TextChanged += TxtPorcentagem_TextChanged;
+            TxtPorcentagem.Enter += TxtPorcentagem_Enter;
+            TxtPorcentagem.Leave += TxtPorcentagem_Leave;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(98, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Porcentagem";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(114, 10);
+            label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(78, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Porcentagem";
             // 
             // TxtOutrosDescontos
             // 
-            this.TxtOutrosDescontos.Location = new System.Drawing.Point(174, 25);
-            this.TxtOutrosDescontos.Name = "TxtOutrosDescontos";
-            this.TxtOutrosDescontos.Size = new System.Drawing.Size(89, 20);
-            this.TxtOutrosDescontos.TabIndex = 2;
-            this.TxtOutrosDescontos.Text = "0,00";
-            this.TxtOutrosDescontos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.TxtOutrosDescontos.TextChanged += new System.EventHandler(this.TxtOutrosDescontos_TextChanged);
-            this.TxtOutrosDescontos.Enter += new System.EventHandler(this.TxtOutrosDescontos_Enter);
-            this.TxtOutrosDescontos.Leave += new System.EventHandler(this.TxtOutrosDescontos_Leave);
+            TxtOutrosDescontos.Location = new System.Drawing.Point(203, 29);
+            TxtOutrosDescontos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            TxtOutrosDescontos.Name = "TxtOutrosDescontos";
+            TxtOutrosDescontos.Size = new System.Drawing.Size(103, 23);
+            TxtOutrosDescontos.TabIndex = 2;
+            TxtOutrosDescontos.Text = "0,00";
+            TxtOutrosDescontos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            TxtOutrosDescontos.TextChanged += TxtOutrosDescontos_TextChanged;
+            TxtOutrosDescontos.Enter += TxtOutrosDescontos_Enter;
+            TxtOutrosDescontos.Leave += TxtOutrosDescontos_Leave;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(171, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Outros Descontos";
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(200, 10);
+            label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(101, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Outros Descontos";
             // 
             // BtnCalcular
             // 
-            this.BtnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnCalcular.Location = new System.Drawing.Point(269, 25);
-            this.BtnCalcular.Name = "BtnCalcular";
-            this.BtnCalcular.Size = new System.Drawing.Size(104, 23);
-            this.BtnCalcular.TabIndex = 3;
-            this.BtnCalcular.Text = "Calcular";
-            this.BtnCalcular.UseVisualStyleBackColor = true;
-            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            BtnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnCalcular.Location = new System.Drawing.Point(314, 29);
+            BtnCalcular.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            BtnCalcular.Name = "BtnCalcular";
+            BtnCalcular.Size = new System.Drawing.Size(121, 27);
+            BtnCalcular.TabIndex = 3;
+            BtnCalcular.Text = "Calcular";
+            BtnCalcular.UseVisualStyleBackColor = true;
+            BtnCalcular.Click += BtnCalcular_Click;
             // 
             // RtxDescricao
             // 
-            this.RtxDescricao.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RtxDescricao.Location = new System.Drawing.Point(12, 61);
-            this.RtxDescricao.Name = "RtxDescricao";
-            this.RtxDescricao.ReadOnly = true;
-            this.RtxDescricao.Size = new System.Drawing.Size(954, 556);
-            this.RtxDescricao.TabIndex = 5;
-            this.RtxDescricao.Text = "";
+            RtxDescricao.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            RtxDescricao.Location = new System.Drawing.Point(14, 70);
+            RtxDescricao.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            RtxDescricao.Name = "RtxDescricao";
+            RtxDescricao.ReadOnly = true;
+            RtxDescricao.Size = new System.Drawing.Size(1112, 641);
+            RtxDescricao.TabIndex = 5;
+            RtxDescricao.Text = "";
             // 
             // BtnDetalhar
             // 
-            this.BtnDetalhar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnDetalhar.Location = new System.Drawing.Point(379, 25);
-            this.BtnDetalhar.Name = "BtnDetalhar";
-            this.BtnDetalhar.Size = new System.Drawing.Size(75, 23);
-            this.BtnDetalhar.TabIndex = 4;
-            this.BtnDetalhar.Text = "Detalhar";
-            this.BtnDetalhar.UseVisualStyleBackColor = true;
-            this.BtnDetalhar.Click += new System.EventHandler(this.BtnDetalhar_Click);
+            BtnDetalhar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnDetalhar.Location = new System.Drawing.Point(442, 29);
+            BtnDetalhar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            BtnDetalhar.Name = "BtnDetalhar";
+            BtnDetalhar.Size = new System.Drawing.Size(88, 27);
+            BtnDetalhar.TabIndex = 4;
+            BtnDetalhar.Text = "Detalhar";
+            BtnDetalhar.UseVisualStyleBackColor = true;
+            BtnDetalhar.Click += BtnDetalhar_Click;
             // 
             // FrmPensao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 629);
-            this.Controls.Add(this.BtnDetalhar);
-            this.Controls.Add(this.RtxDescricao);
-            this.Controls.Add(this.BtnCalcular);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.TxtOutrosDescontos);
-            this.Controls.Add(this.TxtPorcentagem);
-            this.Controls.Add(this.TxtValorBruto);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FrmPensao";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Pensão";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1141, 726);
+            Controls.Add(BtnDetalhar);
+            Controls.Add(RtxDescricao);
+            Controls.Add(BtnCalcular);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(TxtOutrosDescontos);
+            Controls.Add(TxtPorcentagem);
+            Controls.Add(TxtValorBruto);
+            Controls.Add(label1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmPensao";
+            ShowIcon = false;
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Pensão";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
